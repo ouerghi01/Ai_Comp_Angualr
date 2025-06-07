38c0cfe49f79e9b7409a3e2cd776637b8ebccbe6
ouerghi01 <mohamedaziz.ouerghi@etudiant-enit.utm.tn>
2025-06-07 20:41:21 +0100

    add actions v4

--- src/app/ai-comp/chat.spec.ts
+++ src/app/ai-comp/chat.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ChatService } from './chat';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Chat', () => {
   let service: ChatService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+     TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [ChatService]
+  });
     service = TestBed.inject(ChatService);
   });
 
